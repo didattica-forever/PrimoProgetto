fef40dcb9ba9e690fee07f958de96d0491ebdcf8
Paolo M. <didattica@gmx.com>
2025-08-25 13:20:51 +0200

    reading and writing parms completed

--- configuration.py
+++ configuration.py
@@ -1,16 +1,33 @@
 import os
 
 
-class Configuration:
+class Configuration(object):
     HOME_DIRECTORY = os.path.expanduser("~")
     
     WORKING_DIRECTORY = f"{HOME_DIRECTORY}/.passwdgen"
     
     PROFILE = f"{WORKING_DIRECTORY}/passwdgen.profile"
     
+    PASSWORD_LENGTH = None
+    LOWERCASE = None
+    UPPERCASE = None
+    NUMBERS = None
+    SPECIAL = None
+    
+    #
+    # Singleton Pattern for the class
+    #
+    def __new__(cls, *args, **kwargs):
+        # print("__new__")
+        if not hasattr(cls, 'instance'):
+            cls.instance = super(Configuration, cls).__new__(cls)
+        return cls.instance
+    
+    
     def __init__(self):
-        self.check_home_directory()
-        self.check_profile()
+        # print("__init__")
+        self.instance.check_home_directory()
+        self.instance.check_profile()
     
     def check_home_directory(self):
         if not os.path.exists(Configuration.WORKING_DIRECTORY):
@@ -30,11 +47,10 @@ class Configuration:
             file.write("### Passwdgen Profile V 1.0\n")
             file.write("###\n")
             file.write("default_passwd_length 8 # default password length if none specified\n")
-            file.write("default_charset ascii_min,ascii_max,num # default password charset\n")
-            file.write("charset ascii_min qwertyuiopasdfghjklzxcvbnm\n")
-            file.write("charset ascii_max QWERTYUIOPASDFGHJKLZXCVBNM\n")
-            file.write("charset num 1234567890\n")
-            file.write("charset special $?@#+-_\n")
+            file.write("default_charset charset_min,charset_max,charset_num # default password charset\n")
+            file.write("charset_letters qwertyuiopasdfghjklzxcvbnm\n")
+            file.write("charset_num 1234567890\n")
+            file.write("charset_special $?@#+-_\n")
             file.write("\n\n\n\n\n")
             file.write("### END OF PROFILE\n")
             
@@ -43,4 +59,31 @@ class Configuration:
         pass
     
     def read_profile(self):
-        pass
+        with open(Configuration.PROFILE, "r") as file:
+            count = 1
+            for line in file:
+                line = line.strip()
+                if line.startswith('#') or len(line.strip()) == 0:
+                    count += 1
+                    continue
+                
+                self.process_line(line)
+                count += 1
+            
+    def process_line(self, line):
+        arr = line.split(' ')
+        if len(arr) <2:
+            return
+        parm = arr[0].strip()
+        value = arr[1].strip()
+        # print(f"parm={parm} is {value}")
+        if parm == 'default_passwd_length':
+            Configuration.PASSWORD_LENGTH = int(arr[1])        
+        if parm == 'charset_letters':
+            Configuration.LOWERCASE = arr[1].lower()
+            Configuration.UPPERCASE = arr[1].upper()
+        if parm == 'charset_num':
+            Configuration.NUMBERS = arr[1]
+        if parm == 'charset_special':
+            Configuration.SPECIAL = arr[1]
+    
--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from configuration import Configuration
 
-configuration_object = None
+configuration_object = Configuration()
 
 def main():
     print("Hello from passwordgenerator!")
     home_dir = Configuration.HOME_DIRECTORY
     print(f"Home directory is {home_dir}")
     
-    configuration_object = Configuration()
-    
-    
+   # configuration_object = Configuration()
+    print(f"Run parameters:")
+    print(f"\tConfiguration Password Length is {Configuration.PASSWORD_LENGTH}")
+    print(f"\tLowercase charset is {Configuration.LOWERCASE}")
+    print(f"\tUppercase charset is {Configuration.UPPERCASE}")
+    print(f"\tNumbers charset is {Configuration.NUMBERS}")
+    print(f"\tSpecial charset is {Configuration.SPECIAL}")
+
     
     
     
